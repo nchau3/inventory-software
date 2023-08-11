6915e52c8870110f5b5afe11861e2dad904d95ab
Nicholas <nc_chau@hotmail.com>
2023-08-10 21:17:29 -0400

    link to home in sidenav header

--- app/components/sidenav-header.tsx
+++ app/components/sidenav-header.tsx
@@ -1,5 +1,11 @@
+import Link from "next/link";
+
 export default function SideNavHeader() {
     return (
-        <div className="w-fit p-2 text-[24px] text-center border rounded-full text-white bg-slate-800">SOFTWARE</div>
+        <div className="w-fit p-2 text-[24px] text-center border rounded-full text-white bg-slate-800">
+            <Link href={"/"}>
+                SOFTWARE
+            </Link>
+        </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,7 @@ import RootLayout from "./layout";
 export default function Home() {
   return (
     <RootLayout>
-      <main className="page-container">
-        <h1>Home Page!</h1>
-      </main>
+
     </RootLayout>
   )
 }
