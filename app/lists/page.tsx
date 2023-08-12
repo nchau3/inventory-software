9995667a91f8823b4d9e27cea4b6351fa355449f
Nicholas <nc_chau@hotmail.com>
2023-08-12 14:20:51 -0400

    template for lists page

---
+++ app/lists/page.tsx
@@ -0,0 +1,13 @@
+import RootLayout from "../layout";
+
+export default function Lists() {
+    return (
+        <>
+            <RootLayout>
+                <div className="page-container">
+                    <h1 className="text-[60px]">LISTS</h1>
+                </div>
+            </RootLayout>
+        </>
+    )
+}
