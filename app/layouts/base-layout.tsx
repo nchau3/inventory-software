34c2c34a4160bdc4d60ab21615c9a63291580705
Nicholas <nc_chau@hotmail.com>
2023-08-13 21:07:57 -0400

    layout container for nav bars

---
+++ app/layouts/base-layout.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { useState } from "react";
+import type { PropsWithChildren, ReactNode } from "react";
+import s from "./base-layout.module.css";
+
+export default function BaseLayout({
+  topNavSlot: topBarSlot,
+  sideNavSlot: sidebarSlot,
+  children,
+}: PropsWithChildren<{ topNavSlot: ReactNode; sideNavSlot: ReactNode }>) {
+  const [sidebarCollapsed, setSidebarCollapsed] = useState<boolean>(false);
+
+  return (
+    <div className={s.layoutContainer}>
+      <div className={s.topBar}>{topBarSlot}</div>
+      <div
+        className={`${s.sidebarMain} ${
+          sidebarCollapsed ? s.sidebarCollapsed : ""
+        }`}
+      >
+        <div className={s.sidebar}>
+          <button
+            className={s.expandButton}
+            onClick={() => setSidebarCollapsed(!sidebarCollapsed)}
+          >
+            {sidebarCollapsed ? "+" : "-"}
+          </button>
+          {sidebarSlot}
+        </div>
+        <div className={s.main}>{children}</div>
+      </div>
+    </div>
+  );
+}
