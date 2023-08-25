eb001231955f3c399d3ccf4ac53f91b0985335d3
Nicholas <nc_chau@hotmail.com>
2023-08-24 23:11:46 -0400

    add location quantity to query

--- app/items/page.tsx
+++ app/items/page.tsx
@@ -1,11 +1,35 @@
-import { Item } from "@prisma/client";
 import ItemList from "./components/item-list";
 import { prisma } from "../../prisma/client";
+import { Item } from "@prisma/client";
+
+export interface itemWithTotalQuantity extends Item {
+  totalQuantity: number;
+}
 
 const getItems = async () => {
-  const itemsData = await prisma.item.findMany();
+  const itemsData = await prisma.item.findMany({
+    select: {
+      id: true,
+      name: true,
+      sku: true,
+      date_created: true,
+      last_modified: true,
+      is_active: true,
+      locations: {
+        select: {
+          quantity: true
+        }
+      }
+    }
+  });
 
-  return itemsData;
+  return itemsData.map(item => {
+    const totalQuantity = item.locations.reduce((prev, curr) => prev + curr.quantity, 0);
+    return {
+      ...item,
+      totalQuantity
+    }
+  });
 };
 
 export default async function Items() {
