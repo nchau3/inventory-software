66df9ebd55913dadbd9fc5f2a39d1ccf90c20abd
Nicholas <nc_chau@hotmail.com>
2023-08-31 16:13:00 -0400

    increase item seeding

--- prisma/seeds/items.ts
+++ prisma/seeds/items.ts
@@ -2,11 +2,11 @@ import { PrismaClient } from "@prisma/client";
 import { faker } from "@faker-js/faker";
 import { randomNumber } from "../util/random";
 
-const data = Array.from({ length: 100 }).map(() => {
+const data = Array.from({ length: 5000 }).map(() => {
   const fakeName = faker.word.noun({ length: { min: 4, max: 12 } });
   const fakeSku = `${fakeName.slice(0, 4).toUpperCase()}-${(Math.random() * 100)
     .toString()
-    .slice(-4, -1)}`;
+    .slice(-6, -1)}`;
   return { name: fakeName, sku: fakeSku };
 });
 
@@ -16,8 +16,8 @@ export async function seedItems(prisma: PrismaClient) {
   });
 
   //assign items to random locations
-  for (let i = 1; i < 200; i++) {
-    const randomItem = randomNumber(100);
+  for (let i = 1; i < 1000; i++) {
+    const randomItem = randomNumber(300);
     const randomLocation = randomNumber(150);
     const randomQuantity = randomNumber(100);
 
