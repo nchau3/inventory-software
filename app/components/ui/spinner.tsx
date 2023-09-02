2eb8d1ac82cac49cdf21ade16d3d08b82cd940bf
Nicholas <nc_chau@hotmail.com>
2023-09-02 14:23:54 -0400

    stroke-width 2px

--- app/components/ui/spinner.tsx
+++ app/components/ui/spinner.tsx
@@ -1,7 +1,7 @@
 export default function Spinner() {
   return (
     <div
-      className="ml-auto inline-block h-full w-full animate-spin rounded-full border-4 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_0.25s_linear_infinite]"
+      className="ml-auto inline-block h-full w-full animate-spin rounded-full border-2 border-solid border-current border-r-transparent align-[-0.125em] motion-reduce:animate-[spin_0.25s_linear_infinite]"
       role="status"
     >
       <span className="!absolute !-m-px !h-px !w-px !overflow-hidden !whitespace-nowrap !border-0 !p-0 ![clip:rect(0,0,0,0)]"></span>
