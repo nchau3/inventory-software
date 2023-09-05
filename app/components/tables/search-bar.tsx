ae153a4ff237fa6ac3d4cee5080e2e936d1a2c0b
Nicholas <nc_chau@hotmail.com>
2023-09-05 08:12:38 -0400

    add key to input, autofocus

--- app/components/tables/search-bar.tsx
+++ app/components/tables/search-bar.tsx
@@ -13,11 +13,12 @@ export default function SearchBar({ path, value, onChange, isLoading}: Props) {
   return (
       <div className="mb-2 flex h-10 w-full items-center rounded-none border border-slate-400 bg-white px-2 shadow-md focus-within:border-2">
         <input
-          id="searchBar"
+          id={`search-${path}`}
           className="h-full w-11/12 px-2 focus:outline-none"
           value={value}
           onChange={(e) => onChange(e.target.value)}
           placeholder={`Search ${path}...`}
+          autoFocus
         ></input>
         {isLoading && (
           <div className="ml-auto h-6 w-6">
