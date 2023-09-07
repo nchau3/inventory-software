8bcd005b33aba45af9b62b4464deb1d4b1cd014a
Nicholas <nc_chau@hotmail.com>
2023-09-07 12:01:53 -0400

    add path prop for links

--- app/locations/page.tsx
+++ app/locations/page.tsx
@@ -1,6 +1,7 @@
 import { prisma } from "../../prisma/client";
 import Table from "../components/tables/table";
 
+const path = "locations";
 const columns = ["name", "status", "last_modified"];
 
 const getLocations = async () => {
@@ -11,24 +12,26 @@ const getLocations = async () => {
       is_active: true,
       date_created: true,
       last_modified: true,
-      items: {
-        select: {
-          item: true,
-        },
-      },
     },
   });
 
-  return locationsData.map((location) => {
+  const locations = locationsData.map((location) => {
     return {
       ...location,
       columns,
+      path
     };
   });
+
+  return { locations, columns };
 };
 
 export default async function Locations() {
-  const locations = await getLocations();
+  const { locations, columns } = await getLocations();
 
-  return <Table data={{ body: locations, columns: columns }} />;
+  return (
+    <div className="px-10 max-w-[1200px]">
+      <Table body={locations} path={path} columns={columns} />;
+    </div>
+  )
 }
